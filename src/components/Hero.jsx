4cb1b1d86c40f2923a1557a2c785aa9051a5a34b
Szkrishna <krisnas9792@gmail.com>
2024-12-27 12:56:37 +0530

    completed hero section animation from text to image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,16 +17,16 @@ const Hero = () => {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <motion.h1 variants={container(0)} initial="hidden" animate="visible" className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Krishna Singh</motion.h1>
-                        <motion.span variants={container(0)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+                        <motion.h1 variants={container(0.3)} initial="hidden" animate="visible" className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Krishna Singh</motion.h1>
+                        <motion.span variants={container(0.6)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
                             Full Stack Developer
                         </motion.span>
-                        <motion.p variants={container(0)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
+                        <motion.p variants={container(0.9)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex justify-center">
-                        <img  className="" src={profilePic} alt="ks-profile-img" />
+                        <motion.img initial={{x: 100, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1, delay: 1.2}} className="" src={profilePic} alt="ks-profile-img" />
                     </div>
                 </div>
             </div>
