8defe7faff79750e31086b5f05f767d9a6b2ddd5
Szkrishna <krisnas9792@gmail.com>
2024-12-27 22:49:55 +0530

    adding more animation to technologies section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -23,8 +23,8 @@ const iconVarients = (duration) => ({
 const Technologies = () => {
     return (
         <div className="border-b border-neutral-800 pb-24">
-            <h2 className="my-20 text-center text-4xl">Technologies</h2>
-            <div className="flex flex-wrap items-center justify-center gap-4">
+            <motion.h2 whileInView={{opacity: 1, y: 0}} initial={{opacity: 0, y: -100}} transition={{duration: 1.5}} className="my-20 text-center text-4xl">Technologies</motion.h2>
+            <motion.div whileInView={{opacity: 1, x: 0}} initial={{opacity: 0, x: -100}} transition={{duration: 1.5}} className="flex flex-wrap items-center justify-center gap-4">
                 <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
                     <RiReactjsLine className="text-7xl text-cyan-400" />
                 </motion.div>
@@ -43,7 +43,7 @@ const Technologies = () => {
                 <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
                     <BiLogoPostgresql className="text-7xl text-sky-700" />
                 </motion.div>
-            </div>
+            </motion.div>
         </div>
     )
 }
