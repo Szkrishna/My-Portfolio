1f4abf397e5abd0231ccb837cd01ead0ed937157
Szkrishna <krisnas9792@gmail.com>
2025-01-16 16:37:52 +0530

    changing navsection according to my portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/home-img.jpg";
 import {motion} from "framer-motion";
 
 const container = (delay) => ({
@@ -13,7 +13,7 @@ const container = (delay) => ({
 })
 const Hero = () => {
     return (
-        <div className="border-b border-neutral-900 pb-4 lg:mb-36">
+        <div className="border-b border-neutral-900 pb-[24px]">
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
@@ -21,12 +21,12 @@ const Hero = () => {
                         <motion.span variants={container(0.6)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
                             Full Stack Developer
                         </motion.span>
-                        <motion.p variants={container(0.9)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
+                        <motion.p variants={container(0.9)} initial="hidden" animate="visible" className="max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex justify-center">
-                        <motion.img initial={{x: 100, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1, delay: 1.2}} className="" src={profilePic} alt="ks-profile-img" />
+                        <motion.img initial={{x: 100, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1, delay: 1.2}} className="rounded-lg" src={profilePic} alt="ks-profile-img" />
                     </div>
                 </div>
             </div>
