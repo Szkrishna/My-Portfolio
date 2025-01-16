b5b86d5ad2f77d17006aa7881cbec3e7d3fa0383
Szkrishna <krisnas9792@gmail.com>
2025-01-16 17:06:47 +0530

    changing hero section content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,9 +15,9 @@ const Hero = () => {
     return (
         <div className="border-b border-neutral-900 pb-[24px]">
             <div className="flex flex-wrap">
-                <div className="w-full lg:w-1/2">
+                <div className="w-full lg:w-1/2 py-[30px]">
                     <div className="flex flex-col items-center lg:items-start">
-                        <motion.h1 variants={container(0.3)} initial="hidden" animate="visible" className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Krishna Singh</motion.h1>
+                        <motion.h1 variants={container(0.3)} initial="hidden" animate="visible" className="pb-[36px] text-6xl font-thin tracking-tight lg:mt-[24px] lg:text-8xl">Krishna Singh</motion.h1>
                         <motion.span variants={container(0.6)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
                             Full Stack Developer
                         </motion.span>
