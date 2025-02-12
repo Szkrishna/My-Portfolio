67b934e1b0a08eaf00bcb2283aba03d36cb192a4
Szkrishna <krisnas9792@gmail.com>
2025-02-12 18:00:17 +0530

    trachnology section content changed and contact section animation changes

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,11 +4,11 @@ import { motion } from "framer-motion";
 
 const Contacts = () => {
   return (
-    <div className="border-b border-neutral-900 pb-8">
-      <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 0.5 }} className="mb-6 text-center text-4">Get in Touch</motion.h2>
+    <div className="border-b border-neutral-900 pb-12">
+      <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className="mb-12 text-center text-4xl">Get in Touch</motion.h2>
       <div className="text-center tracking-tighter">
-        <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className="my-3">{CONTACT.address}</motion.p>
-        <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className="my-3">{CONTACT.phoneNo}</motion.p>
+        <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1.5 }} className="my-3">{CONTACT.address}</motion.p>
+        <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1.5 }} className="my-3">{CONTACT.phoneNo}</motion.p>
         <a href="#" className="text-gray-100">{CONTACT.email}</a>
       </div>
     </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,10 +1,9 @@
-// import React from 'react';
-import {RiReactjsLine} from "react-icons/ri";
-import {TbBrandNextjs} from "react-icons/tb";
-import {SiMongodb} from "react-icons/si";
-import {DiRedis} from "react-icons/di";
-import {FaNodeJs} from "react-icons/fa";
-import {BiLogoPostgresql} from "react-icons/bi";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import { DiMysql } from "react-icons/di";
+import { FaAngular } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
 import {motion} from "framer-motion";
 
 const iconVarients = (duration) => ({
@@ -25,23 +24,23 @@ const Technologies = () => {
         <div className="border-b border-neutral-300 pb-24">
             <motion.h2 whileInView={{opacity: 1, y: 0}} initial={{opacity: 0, y: -100}} transition={{duration: 1.5}} className="my-20 text-center text-4xl">Technologies</motion.h2>
             <motion.div whileInView={{opacity: 1, x: 0}} initial={{opacity: 0, x: -100}} transition={{duration: 1.5}} className="flex flex-wrap items-center justify-center gap-4">
-                <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-600 p-4">
                     <RiReactjsLine className="text-7xl text-cyan-400" />
                 </motion.div>
-                <motion.div variants={iconVarients(3)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <TbBrandNextjs className="text-7xl" />
+                <motion.div variants={iconVarients(3)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-600 p-4">
+                    <FaAngular className="text-7xl text-red-700" />
                 </motion.div>
-                <motion.div variants={iconVarients(5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <SiMongodb className="text-7xl text-green-500" />
-                </motion.div>
-                <motion.div variants={iconVarients(2)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <DiRedis className="text-7xl text-red-700" />
-                </motion.div>
-                <motion.div variants={iconVarients(6)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div variants={iconVarients(6)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-600 p-4">
                     <FaNodeJs className="text-7xl text-green-500" />
                 </motion.div>
-                <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <BiLogoPostgresql className="text-7xl text-sky-700" />
+                <motion.div variants={iconVarients(2)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-600 p-4">
+                    <DiMysql className="text-7xl text-blue-500" />
+                </motion.div>
+                <motion.div variants={iconVarients(5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-600 p-4">
+                    <SiMongodb className="text-7xl text-green-500" />
+                </motion.div>
+                <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-600 p-4">
+                    <FaPython className="text-7xl text-yellow-500" />
                 </motion.div>
             </motion.div>
         </div>
