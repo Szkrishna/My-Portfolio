b021fcf365d1c179d176644457bce2849a028b35
Szkrishna <krisnas9792@gmail.com>
2025-06-20 23:33:42 +0530

    package and config changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // postcss: './postcss.config.js',
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['krishna-singh-portfolio.tech', 'www.krishna-singh-portfolio.tech'],
+  },
 })
