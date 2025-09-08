9b54e5d5d4f3d1946fdbe15466e9b39be88aba02
Krishna Singh <krishna@oges.com>
2025-09-08 16:32:35 +0530

    hero section changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,24 +18,24 @@ const Hero = () => {
                 <div className="w-full xl:w-1/2">
                     <div className="flex flex-col items-center xl:items-start">
                         <motion.h1 variants={{hidden: { opacity: 0, y: -50 }, visible: { opacity: 1, y: 0, transition: { duration: 1.5 } },}}
-                            initial="hidden" animate="visible" className="pb-[36px] text-5xl font-thin tracking-tight lg:text-6xl xl:text-7xl xxl:text-8xl" style={{ lineHeight: 0.75 }}>
+                            initial="hidden" animate="visible" className="pb-[16px] text-5xl font-thin tracking-tight lg:text-6xl xl:text-7xl xxl:text-8xl" style={{ lineHeight: 0.75 }}>
                             Krishna Singh
                         </motion.h1>
 
                         <motion.span variants={{hidden: { opacity: 0, scale: 0.8 }, visible: { opacity: 1, scale: 1, transition: { duration: 1.5, delay: 0.5 } },}}
-                            initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent ms-1 lg:ms-2">
+                            initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent p-1">
                             Full Stack Developer
                         </motion.span>
 
                         <motion.p variants={{hidden: { opacity: 0, y: 50 }, visible: { opacity: 1, y: 0, transition: { duration: 1.5, delay: 1 } },}}
-                            initial="hidden" animate="visible" className="max-w-xl pb-6 pt-4 lg:py-4 lg:pe-4 font-light tracking-tighter ms-1 lg:ms-2">
+                            initial="hidden" animate="visible" className="max-w-xl p-1 font-light tracking-tighter">
                             {HERO_CONTENT}
                         </motion.p>
                     </div>
                 </div>
 
                 <div className="w-full xl:w-1/2">
-                    <div className="flex justify-center">
+                    <div className="flex justify-center lg:mt-[10px]">
                         <motion.img initial={{x: 100, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1.5, delay: 1.5}} className="rounded-2xl max-h-[370px]" src={profilePic} alt="ks-profile-img" />
                     </div>
                 </div>
@@ -44,4 +44,4 @@ const Hero = () => {
     )
 }
 
-export default Hero
+export default Hero;
