4983a434cb6c65db719198f13502784df6aa5a03
Krishna Singh <krishna@oges.com>
2025-09-08 16:33:02 +0530

    about section changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <h2 className="mb-12 lg:my-20 text-center text-4xl">About
                 <span className="text-neutral-500">Me</span>
             </h2>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap items-center">
                 <motion.div whileInView={{opacity: 1, x: 0}} initial={{opacity: 0, x: -100}} transition={{duration: 0.5}} className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center lg:justify-end">
                         <img className="rounded-2xl" src={aboutImg} alt="about-img" style={{ width: "28rem" }}/>
