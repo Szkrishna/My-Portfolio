2eb66b331becec29a0faa9b3d9c5ade2c2626b2e
Szkrishna <krisnas9792@gmail.com>
2026-06-25 17:20:37 +0530

    hero section content changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/hero-img.jpg";
+import profilePic from "../assets/hero-img.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
